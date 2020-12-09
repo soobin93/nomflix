b365e07e8d80d4cef868bfe8f9ae844c48f9c942
Soobin <ssbin93@gmail.com>
2020-12-09 22:09:57 +1000

    Set up react router switch

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
+import Header from "Components/Header";
 
 class App extends Component {
   render() {
     return (
         <>
-          <Router />
+            <Header />
+            <Router />
         </>
     );
   }
