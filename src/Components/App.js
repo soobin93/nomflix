8dad511cd97786e11503475e973b949309103da8
Soobin <ssbin93@gmail.com>
2020-12-10 22:05:00 +1000

    Add global styles

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,15 @@
 import React, {Component} from "react";
 import Router from "Components/Router";
+import GlobalStyles from "Components/GlobalStyles";
 
 class App extends Component {
   render() {
-    return <Router/>;
+    return (
+      <>
+        <Router/>
+        <GlobalStyles />
+      </>
+    );
   }
 }
 
