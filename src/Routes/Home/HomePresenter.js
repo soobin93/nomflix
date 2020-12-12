71fb2d73959effd85d038a04ff5e5fe9cd31910c
unknown <ssbin93@gmail.com>
2020-12-12 23:53:10 +1000

    Finalising TV and Home Presenters

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,7 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) => null;
+import Loader from "Components/Loader";
+import Section from "Components/Section";
+
+const Container = styled.div`
+    padding: 80px 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) => loading ? <Loader /> : (
+    <Container>
+        {nowPlaying && nowPlaying.length > 0 && (
+            <Section title="Now Playing">
+                { nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>) }
+            </Section>
+        )}
+
+        {upcoming && upcoming.length > 0 && (
+            <Section title="Upcoming Movies">
+                { nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>) }
+            </Section>
+        )}
+
+        {popular && popular.length > 0 && (
+            <Section title="Popular Movies">
+                { nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>) }
+            </Section>
+        )}
+    </Container>
+);
 
 HomePresenter.propTypes = {
     nowPlaying: PropTypes.array,
