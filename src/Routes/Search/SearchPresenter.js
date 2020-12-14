2bdf5b56b3beef82a0400ba6266cc096e0321780
Soobin <ssbin93@gmail.com>
2020-12-14 13:16:40 +1000

    Start working on Search Presenter

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,36 @@
-export default () => "Search";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  loading,
+  searchInput,
+  handleSubmit,
+  error
+}) => (
+  <Container>
+    <Form onSubmit={handleSubmit
+    }>
+
+    </Form>
+  </Container>
+);
+
+SearchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  tvResults:PropTypes.array,
+  searchInput:PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired
+};
+
+export default SearchPresenter;
